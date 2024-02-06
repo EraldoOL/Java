14deb42fde05e3799f0748ab7846b43dd26b46d3
EraldoOL <eo80797@gmail.com>
2024-02-06 15:17:43 -0300

    update Estudos.java

--- Estudos.java
+++ Estudos.java
@@ -2518,3 +2518,144 @@ public class Main{
     }
   }
 }
+
+
+
+
+public class Arvore{
+  
+  
+
+
+public class Elemento<TIPO>{
+  
+  private TIPO valor;
+  
+  private Elemento<TIPO> esquerda;
+  private Elemento<TIPO> direita;
+  
+  
+  
+  public Elemento(TIPO novoValor){
+    
+    this.valor = novoValor;
+    this.esquerda = null;
+    this.direita = null;
+    
+  }
+  
+  
+  public TIPO getValor() {
+    
+    return valor;
+  }
+  
+  public void setValor(TIPO valor){
+    
+    this.valor = valor;
+  }
+  
+  public Elemento<TIPO> getEsquerda() {
+    
+    return esquerda;
+  }
+  
+  public void setEsquerda(Elemento<TIPO> esquerda){
+    
+    this.esquerda = esquerda;
+  }
+  
+  
+  public Elemento<TIPO> getDireita() {
+    
+    return direita;
+  }
+  
+  public void setDireita(Elemento<TIPO> direita){
+    
+    this.direita = direita;
+  }
+  
+  
+}
+
+
+public static class Arvore<TIPO extends Comparable>{
+  
+  
+  private Elemento<TIPO> raiz;
+    
+    
+    public Arvore() {
+      this.raiz = null;
+      
+    
+  }
+  
+  public static void adicionar(TIPO valor){
+    
+    Elemento<TIPO> novoElemento = new Elemento<TIPO>(valor);
+    if (raiz == null) {
+      
+      this.raiz = novoElemento;
+      
+    } else {
+      
+      Elemento<TIPO> atual = this.raiz;
+      while( true ){
+        
+        if (novoElemento.getValor().comparableTO(atual.getValor()) == -1){
+          
+          if (atual.getEsquerda() != null) {
+            atual = atual.getEsquerda();
+            
+          } else {
+            
+            atual.setEsquerda(novoElemento);
+            break;
+          }
+          
+        } else {
+          
+          if (atual.getDireita() != null) {
+            atual = atual.getDireita();
+            
+          } else {
+            
+            atual.setDireita(novoElemento);
+            break;
+          }
+        }
+      }
+    }
+  }
+  
+  public Elemento<TIPO> getRaiz(){
+    
+    return raiz;
+  }
+  
+  
+  public void emOrdem(Elemento<TIPO> atual){
+    
+    if (atual != null) {
+      
+     emOrdem(atual.getEsquerda());
+     System.out.println(atual.getValor());
+     emOrdem(atual.getDireita());
+     
+    }
+    
+  }
+}
+  
+  public static void main(String[] args) {
+    
+    
+    Arvore<Integer> arvore = new Arvore<Integer>();
+    arvore.adicionar(10);
+    arvore.adicionar(5);
+    arvore.emOrdem(arvore.getRaiz());
+    
+  }
+}
